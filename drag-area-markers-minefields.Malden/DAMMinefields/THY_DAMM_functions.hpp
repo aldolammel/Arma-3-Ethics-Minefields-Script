e116e0521b93a94027559121098fc5725ed955b3
Aldo Lammel <aldolammel@gmail.com>
2023-01-30 14:45:16 -0300

    Add files via upload

---
+++ drag-area-markers-minefields.Malden/DAMMinefields/THY_DAMM_functions.hpp
@@ -0,0 +1,26 @@
+// DAMM: DRAG AREA MARKERS MINEFIELDS v1
+// File: your_mission\DAMMinefields\THY_DAMM_functions.hpp
+// by thy (@aldolammel)
+
+class THY_DAMM_functions 
+{
+	tag = "THY"; 
+	
+	class DAMMinefields
+	{
+		file = "DAMMinefields";
+		
+		class DAMM_management
+		{ 
+			preInit = 1;
+		};
+		class DAMM_globalFunctions
+		{ 
+			preInit = 1;
+		};
+		class DAMM_playerLocal
+		{
+			preInit = 1;
+		};
+	};
+};
